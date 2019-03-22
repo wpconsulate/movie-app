7124cd6ac70d27929bcb4155802d73eec0491370
jamesyoung <b6027807@9310-04J522004>
2019-03-22 16:08:29 +0000

    Tiny bugfix
    Changed settings screen path so it works now on tab navigation

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -14,7 +14,7 @@ import Review from '../components/ReviewTab'
 import ProfileWatchlist from '../containers/ProfileWatchlist'
 import Authentication from '../api/Authentication'
 import SetOfUsers from '../api/Collection/SetOfUsers'
-//import SettingsScreens from '../screens/SettingsScreen'
+import SettingsScreens from './SettingsScreen'
 import { Button, Spinner } from 'native-base'
 import UserStore from '../stores/UserStore'
 
@@ -263,6 +263,7 @@ const TabNavigator = createBottomTabNavigator(
     All: { screen: ProfileScreen },
     Friends: { screen: FriendsList },
     Review: { screen: ReviewsList },
+    Settings: SettingsScreens
   },
   {
     tabBarOptions: {
