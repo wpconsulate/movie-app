b6819b0002ffc90f88f8c64b66a3ebbd95663e65
james <james@youngonline.eu>
2019-03-24 23:48:37 +0000

    Qr functionality
    QR functionality
    -User generates a QR code with the data being a films "ID" and then the app picks it up

--- src/components/QrReader.tsx
+++ src/components/QrReader.tsx
@@ -1,25 +1,37 @@
 import React, { Component } from 'react'
-import { Text, View, StyleSheet, Alert } from 'react-native'
-import { Constants, BarCodeScanner, Permissions } from 'expo'
-
-interface IProps {
-    show: boolean
-}
+import { Alert, View, Text, Button, ActivityIndicator, Image } from 'react-native'
+import {  BarCodeScanner, Permissions } from 'expo'
+import { withNavigation } from 'react-navigation'
+import { NavigationScreenProps } from 'react-navigation'
+import { SetOfMovies } from '../api'
+import { Container, Row } from 'native-base';
+interface IProps extends NavigationScreenProps {}
 
 interface IState {
     hasCameraPermission: boolean
+    filmName: string
+    movieId: number
+    isLoading: boolean
+    findFilm: boolean
+    filmURL: string
 }
 
-export default class App extends Component <IProps, IState> {
+class QrReader extends Component <IProps, IState> {
     constructor(props: any) {
         super(props)    
         this.state = {
-            hasCameraPermission: false
+            hasCameraPermission: false,
+            filmName: '',
+            movieId: 0,
+            isLoading: true,
+            findFilm: true,
+            filmURL: ''
         }    
     }
 
   componentDidMount() {
     this._requestCameraPermission()
+    this.setState({ isLoading: false, findFilm: true })
   }
 
   _requestCameraPermission = async () => {
@@ -28,37 +40,106 @@ export default class App extends Component <IProps, IState> {
       hasCameraPermission: status === 'granted'
     })
   }
+  onPressItem = (movieId: number) => {
+    this.props.navigation.push('Movie', { movieId })
+  }
 
-  _handleBarCodeRead = data => {
+   _handleBarCodeRead  =  async qrCode => {
     Alert.alert(
       'Scan successful!',
-      JSON.stringify(data)
+      JSON.stringify(qrCode)
     )
-    const test = JSON.stringify(data)
-    console.log(test)
+    let movieTitle = ''
+    let test
+    let poster = ''
+    let test2 = ''
+    const film = qrCode.data
+
+    if (film !== undefined) {
+      movieTitle = await new SetOfMovies().getTitleById(film)
+    }
+
+    if (film !== undefined) {
+      test = await new SetOfMovies().findMovieById(film)
+      test2 = test.title
+      poster = test.poster_path
+    }
+    console.log(poster)
+    console.log(test2)
+    this.setState({ 
+      isLoading: false, 
+      movieId: film, 
+      filmName: movieTitle, 
+      findFilm: false,
+      filmURL: poster 
+    })
   }
 
   render() {
-    const { show } = this.props
+    const { filmName, movieId, hasCameraPermission, isLoading, findFilm, filmURL  } = this.state
 
-    if (!show){
-        return(
-            <View/>
-        )
+    if (isLoading) {
+      return (
+        <View><ActivityIndicator /></View>
+      )
     }
 
     return (
-      <View style={{ backgroundColor: '#12152D' }}>
-        {this.state.hasCameraPermission === undefined ?
-          <Text>Requesting for camera permission</Text> :
-          this.state.hasCameraPermission === false ?
-            <Text>Camera permission is not granted</Text> :
-            <BarCodeScanner
-                onBarCodeScanned={this._handleBarCodeRead}
-                style={{ height: 300, width: 300 }}
-            />
+      <Container style={{ flex: 1, backgroundColor: '#12152D'  }} >
+        <Row style={{ alignItems: 'center', justifyContent: 'center' }} >
+        {console.log(findFilm)}
+
+        { !findFilm
+        ? <View><Image 
+        style={{width: 150, height: 300}}
+        source={{ uri: 'http://image.tmdb.org/t/p/w185/' + filmURL }} />
+        </View>
+        :  (hasCameraPermission === undefined ?
+            <Text>Requesting for camera permission</Text> :
+            hasCameraPermission === false ?
+              <Text>Camera permission is not granted</Text> :
+              <BarCodeScanner
+                  onBarCodeScanned={this._handleBarCodeRead}
+                  style={{ height: 200, width: 200, marginTop: 20 }}
+              />
+          )
         }
-      </View>
+
+        </Row> 
+        <Row style={{ flex: 1, flexDirection: 'column', alignSelf: 'center', justifyContent: 'center' }}>
+        { !findFilm && <Button title={'Search Again'} onPress={() => this.setState({ findFilm: !findFilm }) }/>}
+        
+        <Text
+            style={{
+              color: 'white',
+              fontFamily: 'PoppinsMedium',
+              fontSize: 18,
+              marginTop: 10 
+            }}
+        > 
+        {filmName}
+        </Text>
+        <Button
+          title="GO TO FILM"
+          onPress={() => this.onPressItem(movieId)}
+        >
+          <Text
+            style={{
+              fontFamily: 'PoppinsMedium',
+              color: 'black',
+              fontSize: 14,
+              fontWeight: 'bold'
+            }}
+          >
+            GO TO FILM
+          </Text>
+        </Button>
+
+
+        </Row>
+      </Container>
     )
   }
 }
+
+export default withNavigation(QrReader);
